00f9edc2f857224b41b1449eaded48b8cea04da4
hiroto <ff.king.hiroto@gmail.com>
2020-01-27 23:35:07 +0900

    サンプル追加

---
+++ AtCoder_Biginners_Selection/product.hs
@@ -0,0 +1,18 @@
+{-
+問題：
+  二つの正整数a,bの積が偶数か奇数か判定しなさい。
+制約：
+  * 1 <= a,b <= 10000
+  * a,bは整数
+入力：
+  a b
+出力：
+  積が奇数なら'Odd'と、偶数なら'Even'と出力
+-}
+
+main :: IO ()
+main = do
+  [a, b] <- map read . words <$> getLine
+  if mod 2 (a * b) == 0
+  then putStrLn "Even"
+  else putStrLn "Odd"
---
+++ AtCoder_Biginners_Selection/welcomeToAtCoder.hs
@@ -0,0 +1,24 @@
+{-
+問題：
+  整数a,b,cと文字列sが与えられます。a+b+cの計算結果と文字列sを並べて表示しなさい。
+制約：
+  * 1 <=  a,b,c <= 1000
+  * 1 <= |s| <= 100
+入力：
+  a
+  b c
+  s
+出力：
+  a+b+c s\n
+-}
+
+main :: IO ()
+main = do
+  -- 整数の入力
+  a <- readLn
+  -- スペース区切りの整数の入力
+  [b, c] <- map (read :: String -> Int) . words <$> getLine
+  s <- getLine
+  
+  putStrLn $ show (a + b + c) ++ " " ++ s
+
